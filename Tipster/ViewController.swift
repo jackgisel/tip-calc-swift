8de18d551b2ade48c5681ea8e154c157e7d4916e
Jack Gisel <jackrgisel@gmail.com>
2018-12-15 16:24:15 -0800

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Jack Gisel on 12/15/18.
+//  Copyright © 2018 Jack Gisel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
